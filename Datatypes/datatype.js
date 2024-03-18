dd67b0c03b826d057477bdf2fc22f354dea433d3
Rishabh <rishabh2002chauhan@gmail.com>
2024-03-18 10:27:02 +0530

    Noting some datatype

---
+++ Datatypes/datatype.js
@@ -0,0 +1,21 @@
+"use strict";   // treat all JS code as newer version of JS.
+
+//alert(2+3) // we are using node.js, not bowser and this is allowed in node.js but syntax is diffrent.
+
+console.log(5 +
+    8)       // this will work but this is not a good practice to write a code.
+            // code redibility should be high
+
+    console.log("THis is a js code")
+
+    // number => limit is 2 to power 53
+    // big int
+    // string => " "
+    // boolean => true/flase
+    // null => standalone value it means that no value is assigned to that variable. it does not means undefined.
+    // undefined => it means that no value is assigned and is undefined.
+    // symbol => unique
+    // Object 
+
+    console.log(typeof null);           // this is imporatnt point for interview it's type is "Object".
+    console.log(typeof undefined);      // this is important point for interview it's typr is "Undefined".
