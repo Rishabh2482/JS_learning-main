d15a745ed7b5c87b6d1ff41c828404348c4e19b0
Rishabh <rishabh2002chauhan@gmail.com>
2024-03-18 10:12:52 +0530

    Woeking with variables

---
+++ variables/const,variables.js
@@ -0,0 +1,11 @@
+const account=2344;
+let name1="Rishabh"
+var val=true;
+city="Sonebhadra"
+const hero=24;     // const declration must be initialized.
+let num;    // let variable need not be initalized than it will take value as undefined;
+
+console.table([account,name1,val,city,hero,num]);
+console.log(account,name1,val,city);
+
+/* Do not use use var just because it does not diffrenciate between the scope of variables. */
