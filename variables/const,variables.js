c7f237d87a433b393760b7a07332b8e9e8c23c30
Rishabh <rishabh2002chauhan@gmail.com>
2024-03-26 18:42:52 +0530

    Comment added in let,var,const file

--- variables/const,variables.js
+++ variables/const,variables.js
@@ -4,7 +4,11 @@ var val=true;
 city="Sonebhadra"
 const hero=24;     // const declration must be initialized.
 let num;    // let variable need not be initalized than it will take value as undefined;
-
+/*
+ If you declare a variable without using one of these keywords {let,var,const}, 
+ the variable will be automatically declared as a global variable.
+  This can lead to errors if you accidentally overwrite a global variable.
+*/
 console.table([account,name1,val,city,hero,num]);
 console.log(account,name1,val,city);
 
