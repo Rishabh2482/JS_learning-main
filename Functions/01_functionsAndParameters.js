282a7cc9e6962a8cd89b1779f5c014ec14fb183a
Rishabh <rishabh2002chauhan@gmail.com>
2024-04-06 19:32:08 +0530

    Basic function.

---
+++ Functions/01_functionsAndParameters.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(num1,num2,num3)
+{
+    console.log(num1+num2+num3);
+    return num1+num2
+}
+// addTwoNumbers(2,4,"4")  output -->> 64
+// addTwoNumbers("3",4,6)  outpur--->> 346
+// addTwoNumbers(3,null)        output-->> NaN
+addTwoNumbers()
