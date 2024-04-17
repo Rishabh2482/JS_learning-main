0bc62e1ad3212f28df0ae1e141081e8ee49eb79d
Rishabh <rishabh2002chauhan@gmail.com>
2024-04-17 23:08:08 +0530

    Corrected one mistake .

--- 02_object.js
+++ 02_object.js
@@ -32,10 +32,9 @@ const obj3={a:5,b:6,c:7};
 
 const ansobj={obj1,obj2};    //This wiil output as -->>    { obj1: { '1': 'a', '2': 'b' }, obj2: { a: 1, b: 2, c: 3 } }
 
-const newobj1={...obj1,...obj2,...obj3};  //This wiil output as -->>  { '1': 'a', '2': 'b', a: 1, b: 2, c: 3 }
-
+const newobj1={...obj1,...obj2,...obj3};  //This wiil output as -->>  { '1': 'a', '2': 'b', A: 1, B: 2, c: 7, a: 5, b: 6 }
 const newobj2=Object.assign({},obj1,obj2,obj3)   //This wiil output as -->>{ '1': 'a', '2': 'b', A: 1, B: 2, c: 7, a: 5, b: 6 }
-// console.log((newobj2));
+console.log((newobj1));
 
 
 // const trying={a:  obj2.a + obj2.b};
