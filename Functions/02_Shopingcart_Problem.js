1842f80e29e5b68149f7b69c5d75381778c8a94b
Rishabh2482 <rishabh2002chauhan@gmail.com>
2024-05-02 23:37:37 +0530

    Complete this solution for shoping-cart.

--- Functions/02_Shopingcart_Problem.js
+++ Functions/02_Shopingcart_Problem.js
@@ -2,30 +2,31 @@
 the user, but we do not know how many cart items are added we do not know directly so in that case it is important
 to handle such kind of dynamic parameters being passed. so  to solve such problem we use rest or spread operator */
 
-function calculateCartPrise(num1,num2,...num)
-{
-    console.log(num1,num2);
+function calculateCartPrise(num1, num2, ...num) {
+    console.log(num1, num2);
     return num;
+
+    // let newarr=Array.of(num1,num2,num).flat(3);
+    // return (newarr.length)
 }
-console.log(calculateCartPrise(3,4,5,6,7,8));
+console.log(calculateCartPrise(3, 4, 5, 6, 7, 8));
 // Using above function we can able to atlest create an array of all data parametes being passed now we can add all these value by applying loop over the created array.
 
 
 // Handling the objects being passed as function parameter.
-function handlingObjects(anyObject)
-{
-    return(`This is ${anyObject.name} and cart prise is ${anyObject.prise}`);
+function handlingObjects(anyObject) {
+    return (`This is ${anyObject.name} and cart prise is ${anyObject.prise}`);
 }
 console.log(handlingObjects({
-    name:"Rishabh",
-    prise:690
+    name: "Rishabh",
+    prise: 690
 }))
 
 // Handling the Array's being passed as function parameter.
-const myArray=[200,300,400,500]
-function returnSecondValue(getArray)
-{
+const myArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray) {
     return getArray[1];
 }
 
-// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue(myArray));
